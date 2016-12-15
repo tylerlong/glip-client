5c3bc76e8fc821f3b4dfb289dcb6b7683bf64ddb
Tyler Long <tyler4long@gmail.com>
2016-12-15 16:46:00 +0800

    Get subscription correct

--- src/posts.js
+++ src/posts.js
@@ -9,8 +9,9 @@ class Posts {
 
   subscribe(callback) {
     this.subscription = this.rc.createSubscription();
-    this.subscription.on(this.subscription.events.notification, function (msg) {
-      callback(msg.body.post);
+    this.subscription.on(this.subscription.events.notification, function (notification) {
+      // console.log(notification);
+      callback(notification.body);
     });
     this.subscription.setEventFilters(['/restapi/v1.0/account/~/extension/~/glip/posts']).register();
   }
