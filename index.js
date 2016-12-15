ad0a7a14077f481995931c61192a44277599e637
Tyler Long <tyler4long@gmail.com>
2016-12-15 10:02:47 +0800

    Move code to src folder

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-const RingCentral = require('ringcentral');
-
-
-class GlipClient {
-    constructor(options) {
-        this.ringCentral = new RingCentral(options);
-        this.platform = this.ringCentral.platform();
-    }
-
-    login(options) {
-        return this.platform.login(options);
-    }
-
-    post(groupId, text) {
-        return this.platform.post('/glip/posts', { groupId, text });
-    }
-
-    listen(callback) {
-        this.subscription = this.ringCentral.createSubscription('https://platform.ringcentral.com/restapi/v1.0/subscription');
-        this.subscription.on(this.subscription.events.notification, function(msg) {
-            callback(msg.body.post);
-        });
-        this.subscription.setEventFilters(['/restapi/v1.0/account/~/extension/~/glip/posts']).register();
-    }
-}
+const GlipClient = require('./src/glip-client');
 
 
 module.exports = GlipClient;
