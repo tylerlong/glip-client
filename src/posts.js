6244f399dc52703a3923cd5da9cae00035df8392
Tyler Long <tyler4long@gmail.com>
2016-12-15 17:47:34 +0800

    Make Posts get working

--- src/posts.js
+++ src/posts.js
@@ -7,10 +7,18 @@ class Posts {
     return this.rc.platform().post('/glip/posts', options);
   }
 
+  get(options) {
+    if (options.postId) {
+      return this.rc.platform().get(`/glip/posts/${options.postId}`);
+    } else {
+      return this.rc.platform().get('/glip/posts', options);
+    }
+  }
+
   subscribe(callback) {
     this.subscription = this.rc.createSubscription();
     this.subscription.on(this.subscription.events.notification, function (notification) {
-      // console.log(notification);
+      console.log(notification);
       callback(notification.body);
     });
     this.subscription.setEventFilters(['/restapi/v1.0/account/~/extension/~/glip/posts']).register();
