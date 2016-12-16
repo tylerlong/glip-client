30146bc4523d0e6602c788089d8c1837cc16eef9
Tyler Long <tyler4long@gmail.com>
2016-12-16 17:22:29 +0800

    Try to build bundle with webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: __dirname,
+    filename: "index.bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'stage-0'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new uglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+  ],
+};
